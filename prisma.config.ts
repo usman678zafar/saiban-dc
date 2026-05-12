3061fcd9fa7db4ddf1f3b85b88e6beb69ae29056
Muhammad Usman <usman678zafar@gmail.com>
2026-05-12 12:46:57 +0500

    build fixed

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,9 @@
 import 'dotenv/config';
-import defineConfig from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
-    provider: 'postgresql',
     url: process.env.DATABASE_URL,
   },
 });
